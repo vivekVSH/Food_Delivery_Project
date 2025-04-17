0d7ac65d7aa290fe4096fa95b34acc650c8156e4
vivekVSH <handevivek942@gmail.com>
2025-04-17 19:48:26 +0530

    update cart page fully functional

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,40 +1,47 @@
 import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContex = createContext(null)
+export const StoreContex = createContext(null);
 
 const StoreContexProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
 
-    const [cartItems, setCartItems] = useState({});
-
-    const addToCart = (itemId) => {
-        if(!cartItems[itemId]){
-            setCartItems((prev)=>({...prev,[itemId]:1}))
-        }else{
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-        }
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+  };
 
-    const removeFromCart = (itemId) => {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
     }
+    return totalAmount;
+  }
 
-    useEffect(()=> {
-        console.log(cartItems); 
-    },[cartItems])
+  const contextValue = {
+    food_list,
+    cartItems,
+    addToCart,
+    setCartItems,
+    removeFromCart,
+    getTotalCartAmount
+  };
+  return (
+    <StoreContex.Provider value={contextValue}>
+      {props.children}
+    </StoreContex.Provider>
+  );
+};
 
-    const contextValue = {
-        food_list,
-        cartItems,
-        addToCart,
-        setCartItems,
-        removeFromCart
-    }
-    return(
-        <StoreContex.Provider value={contextValue}>
-            {props.children}
-        </StoreContex.Provider>
-    )
-}
-
-export default  StoreContexProvider;
+export default StoreContexProvider;
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,73 @@
-import React from 'react'
-import './Cart.css'
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContex } from "../../Context/StoreContext";
 
 const Cart = () => {
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContex);
   return (
-    <div>
-      
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Item</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>₹{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>₹{item.price * cartItems[item._id]}</p>
+                  <p onClick={()=>removeFromCart(item._id)} className='cross'>X</p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+           <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>₹{getTotalCartAmount()}</p>
+           </div>
+           <hr />
+           <div className="cart-total-details">
+            <p>Delivery Fee</p>
+            <p>₹{20}</p>
+           </div>
+           <hr />
+           <div className="cart-total-details">
+            <b>Total</b>
+            <b>₹{getTotalCartAmount()+20}</b>
+            </div>
+          </div>
+          <button>Proceed To Checkout</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code, Enter it here.</p>
+            <div className='cart-promocode-input'>
+              <input type="text" placeholder='Promo Code'/>
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Cart
+export default Cart;
